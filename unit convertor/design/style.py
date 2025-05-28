8207a15b71ad5a38cc20dfcd5a82d209680d7519
ＫムＫムＲＯＴ <126609857+JANAB-KAKAROT@users.noreply.github.com>
2025-05-28 11:50:16 +0500

    Update style.py

--- unit convertor/design/style.py
+++ unit convertor/design/style.py
@@ -1,6 +1,7 @@
-import streamlit as st
 import os
+import streamlit as st
 
 def load_custom_css():
-    with open(os.path.join("design", "style.css")) as f:
+    css_path = os.path.join(os.path.dirname(__file__), "style.css")
+    with open(css_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
